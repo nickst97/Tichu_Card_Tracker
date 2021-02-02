89df632973ca2a6e0b4943144b959a4af53c8d33
Nick Stavrakakis <nicktheking97@gmail.com>
2021-02-02 21:21:13 +0200

    Background Script Completed

--- background.js
+++ background.js
@@ -1,50 +1,50 @@
 setInterval(function () {
-  // chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  //   let url = tabs[0].url;
-  //   if (!url.includes("www.dod.gr")) {
-  //     chrome.storage.local.set({
-  //       RemainingCards: "null",
-  //       LastRoundName: "null",
-  //     });
-  //   }
-  // });
-
   if (document.getElementById("txtRound") !== null) {
-    var CurrentRound = document.getElementById("txtRound").innerHTML;
-    if (CurrentRound !== "") {
-      chrome.storage.local.get("LastRoundName", function (result) {
-        if (
-          CurrentRound !== result.LastRoundName ||
-          document.getElementById("txtRoundTimer").innerHTML === "00:00:00" ||
-          document.getElementById("txtRoundTimer").innerHTML === "00:00:01" ||
-          document.getElementById("txtRoundTimer").innerHTML.trim() ===
-            "8:88:88" ||
-          document.getElementById("txtRoundTimer").innerHTML === ""
-        ) {
-          chrome.storage.local.set({
-            RemainingCards:
-              "Mahjong,Dogs,Dragon,PM,2s,2h,2d,2c,3s,3h,3d,3c,4s,4h,4d,4c,5s,5h,5d,5c,6s,6h,6d,6c,7s,7h,7d,7c,8s,8h,8d,8c,9s,9h,9d,9c,Ts,Th,Td,Tc,Js,Jh,Jd,Jc,Qs,Qh,Qd,Qc,Ks,Kh,Kd,Kc,As,Ah,Ad,Ac,",
-
-            LastRoundName: CurrentRound,
-          });
-        } else {
-          var TableCards = document.querySelectorAll("div.card.notswipe");
-          chrome.storage.local.get("RemainingCards", function (result) {
-            if (!chrome.runtime.error) {
-              var RemainingCards = result.RemainingCards;
-              for (var i = 0; i < TableCards.length; i++) {
-                RemainingCards = RemainingCards.replace(
-                  TableCards[i].getAttribute("card") + ",",
-                  ""
-                );
-              }
-            }
-            chrome.storage.local.set({
-              RemainingCards: RemainingCards,
-            });
-          });
-        }
+    if (
+      document.querySelectorAll("div.card.notswipe").length === 14 &&
+      document.getElementById("trading").getAttribute("style") === ""
+    ) {
+      chrome.storage.local.set({
+        RemainingCards:
+          "Mahjong,Dogs,PM,Dragon,2s,2h,2d,2c,3s,3h,3d,3c,4s,4h,4d,4c,5s,5h,5d,5c,6s,6h,6d,6c,7s,7h,7d,7c,8s,8h,8d,8c,9s,9h,9d,9c,Ts,Th,Td,Tc,Js,Jh,Jd,Jc,Qs,Qh,Qd,Qc,Ks,Kh,Kd,Kc,As,Ah,Ad,Ac,",
       });
+    } else {
+      var CurrentRound = document.getElementById("txtRound").innerHTML;
+      if (CurrentRound !== "") {
+        chrome.storage.local.get("LastRoundName", function (result) {
+          if (
+            CurrentRound !== result.LastRoundName ||
+            document.getElementById("txtRoundTimer").innerHTML === "00:00:00" ||
+            document.getElementById("txtRoundTimer").innerHTML === "00:00:01" ||
+            document.getElementById("txtRoundTimer").innerHTML.trim() ===
+              "8:88:88" ||
+            document.getElementById("txtRoundTimer").innerHTML === ""
+          ) {
+            chrome.storage.local.set({
+              RemainingCards:
+                "Mahjong,Dogs,PM,Dragon,2s,2h,2d,2c,3s,3h,3d,3c,4s,4h,4d,4c,5s,5h,5d,5c,6s,6h,6d,6c,7s,7h,7d,7c,8s,8h,8d,8c,9s,9h,9d,9c,Ts,Th,Td,Tc,Js,Jh,Jd,Jc,Qs,Qh,Qd,Qc,Ks,Kh,Kd,Kc,As,Ah,Ad,Ac,",
+
+              LastRoundName: CurrentRound,
+            });
+          } else {
+            var TableCards = document.querySelectorAll("div.card.notswipe");
+            chrome.storage.local.get("RemainingCards", function (result) {
+              if (!chrome.runtime.error) {
+                var RemainingCards = result.RemainingCards;
+                for (var i = 0; i < TableCards.length; i++) {
+                  RemainingCards = RemainingCards.replace(
+                    TableCards[i].getAttribute("card") + ",",
+                    ""
+                  );
+                }
+              }
+              chrome.storage.local.set({
+                RemainingCards: RemainingCards,
+              });
+            });
+          }
+        });
+      }
     }
   }
-}, 10);
+}, 5);
--- popup.js
+++ popup.js
@@ -1,68 +1,100 @@
 document.body.onload = function () {
-  chrome.storage.local.get("RemainingCards", function (result) {
-    if (!chrome.runtime.error) {
-      var RemainingCards = result.RemainingCards.split(",");
-      var HTMLcode = "<h2>";
-      var PrevCardNumber = "";
-      RemainingCards.forEach(function (Card) {
-        if (Card === "Mahjong") {
-          HTMLcode += " " + "🀄";
-        } else if (Card === "Dogs") {
-          HTMLcode += " " + "🐕";
-        } else if (Card === "Dragon") {
-          HTMLcode += " " + "🐉";
-        } else if (Card === "PM") {
-          HTMLcode += " " + "🐦";
-        } else {
-          if (Card.charAt(0) !== PrevCardNumber) {
-            HTMLcode += "<br>";
-            PrevCardNumber = Card.charAt(0);
-          }
-          if (Card.charAt(1) === "s") {
-            HTMLcode += ' <span style="color:blue">';
-            if (Card.charAt(0) === "T") {
-              HTMLcode += " 10";
+  chrome.tabs.getAllInWindow(null, function (tabs) {
+    var flag = true;
+    for (var i = 0; i < tabs.length; i++) {
+      if (
+        tabs[i].url.includes("www.dod.gr") ||
+        tabs[i].url.includes("apps.facebook.com/dodcommunity")
+      ) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      chrome.storage.local.set({
+        RemainingCards:
+          "Mahjong,Dogs,PM,Dragon,2s,2h,2d,2c,3s,3h,3d,3c,4s,4h,4d,4c,5s,5h,5d,5c,6s,6h,6d,6c,7s,7h,7d,7c,8s,8h,8d,8c,9s,9h,9d,9c,Ts,Th,Td,Tc,Js,Jh,Jd,Jc,Qs,Qh,Qd,Qc,Ks,Kh,Kd,Kc,As,Ah,Ad,Ac,",
+      });
+    }
+  });
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    if (
+      url.includes("www.dod.gr") ||
+      url.includes("apps.facebook.com/dodcommunity")
+    ) {
+      chrome.storage.local.get("RemainingCards", function (result) {
+        if (!chrome.runtime.error) {
+          var RemainingCards = result.RemainingCards.split(",");
+          var HTMLcode = "<h2>";
+          var PrevCardNumber = "";
+          RemainingCards.forEach(function (Card) {
+            if (Card === "Mahjong") {
+              HTMLcode += " " + "🀄";
+            } else if (Card === "Dogs") {
+              HTMLcode += " " + "🐕";
+            } else if (Card === "Dragon") {
+              HTMLcode += " " + "🐉";
+            } else if (Card === "PM") {
+              HTMLcode += " " + "🐦";
             } else {
-              HTMLcode += ' <span style="letter-spacing: 5px">';
-              HTMLcode += Card.charAt(0);
-              HTMLcode += " </span>";
+              if (Card.charAt(0) !== PrevCardNumber) {
+                HTMLcode += "<br>";
+                PrevCardNumber = Card.charAt(0);
+              }
+              if (Card.charAt(1) === "s") {
+                HTMLcode += ' <span style="color:blue">';
+                if (Card.charAt(0) === "T") {
+                  HTMLcode += " 10";
+                } else {
+                  HTMLcode += ' <span style="letter-spacing: 5px">';
+                  HTMLcode += Card.charAt(0);
+                  HTMLcode += " </span>";
+                }
+                HTMLcode += " </span>";
+              } else if (Card.charAt(1) === "h") {
+                HTMLcode += ' <span style="color:red">';
+                if (Card.charAt(0) === "T") {
+                  HTMLcode += " 10";
+                } else {
+                  HTMLcode += ' <span style="letter-spacing: 5px">';
+                  HTMLcode += Card.charAt(0);
+                  HTMLcode += " </span>";
+                }
+                HTMLcode += " </span>";
+              } else if (Card.charAt(1) === "d") {
+                HTMLcode += ' <span style="color:black">';
+                if (Card.charAt(0) === "T") {
+                  HTMLcode += " 10";
+                } else {
+                  HTMLcode += ' <span style="letter-spacing: 5px">';
+                  HTMLcode += Card.charAt(0);
+                  HTMLcode += " </span>";
+                }
+                HTMLcode += " </span>";
+              } else if (Card.charAt(1) === "c") {
+                HTMLcode += ' <span style="color:green">';
+                if (Card.charAt(0) === "T") {
+                  HTMLcode += " 10";
+                } else {
+                  HTMLcode += ' <span style="letter-spacing: 5px">';
+                  HTMLcode += Card.charAt(0);
+                  HTMLcode += " </span>";
+                }
+                HTMLcode += " </span>";
+              }
             }
-            HTMLcode += " </span>";
-          } else if (Card.charAt(1) === "h") {
-            HTMLcode += ' <span style="color:red">';
-            if (Card.charAt(0) === "T") {
-              HTMLcode += " 10";
-            } else {
-              HTMLcode += ' <span style="letter-spacing: 5px">';
-              HTMLcode += Card.charAt(0);
-              HTMLcode += " </span>";
-            }
-            HTMLcode += " </span>";
-          } else if (Card.charAt(1) === "d") {
-            HTMLcode += ' <span style="color:black">';
-            if (Card.charAt(0) === "T") {
-              HTMLcode += " 10";
-            } else {
-              HTMLcode += ' <span style="letter-spacing: 5px">';
-              HTMLcode += Card.charAt(0);
-              HTMLcode += " </span>";
-            }
-            HTMLcode += " </span>";
-          } else if (Card.charAt(1) === "c") {
-            HTMLcode += ' <span style="color:green">';
-            if (Card.charAt(0) === "T") {
-              HTMLcode += " 10";
-            } else {
-              HTMLcode += ' <span style="letter-spacing: 5px">';
-              HTMLcode += Card.charAt(0);
-              HTMLcode += " </span>";
-            }
-            HTMLcode += " </span>";
-          }
+          });
+          HTMLcode += "</h2>";
+          document.getElementById("RemainingCards").innerHTML = HTMLcode;
         }
       });
-      HTMLcode += "</h2>";
-      document.getElementById("RemainingCards").innerHTML = HTMLcode;
+    } else {
+      var tmp =
+        "<h3 style='margin:5px';> Παίξε Tichu! </h3>" +
+        "<a href='https://apps.facebook.com/dodcommunity'  target='_blank'><img src='facebook-black-24dp.svg' title='Dod Facebook' style='margin:5px';</a>" +
+        "<a href='http://dod.gr/'  target='_blank'><img src='language-black-24dp.svg' title='Dod Website' style='margin:5px';></a>";
+      document.getElementById("PlayTichu").innerHTML = tmp;
     }
   });
 };
